d5048ffe1820b06491a8f8c90ad9b27f797e0c1a
Sara Fremd <saraf@nbs.tech>
2026-03-15 22:17:50 +0200

    format

--- src/auth/auth.utils.ts
+++ src/auth/auth.utils.ts
@@ -1,16 +1,17 @@
-import { Paths } from "../routes/paths"
-import axios from "../services/axios"
+import { Paths } from '../routes/paths';
+import axios from '../services/axios';
 
 export const setSession = (token: string) => {
-    localStorage.setItem('token', token)
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`
-}
+  localStorage.setItem('token', token);
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
 export const getSession = () => {
-    const token = localStorage.getItem('token')
-    return token
-}
+  const token = localStorage.getItem('token');
+  return token;
+};
 
 export const removeSession = () => {
-    localStorage.removeItem('token')
-    location.href = Paths.login
-}
+  localStorage.removeItem('token');
+  location.href = Paths.login;
+};
