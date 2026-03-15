8b8f76a480f574b9527d6150fb5d26100fcc03e2
Sara Fremd <saraf@nbs.tech>
2026-03-15 23:02:47 +0200

    fix AuthContext

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -1,50 +1,57 @@
-import { createContext, useEffect, useCallback, useState, type ReactNode } from "react";
-import type { UserType } from "../types/user.types";
-import { getSession, setSession } from "./auth.utils";
-import { loginByToken } from "../services/auth.service";
+import { createContext, useState, type ReactNode, useEffect } from 'react';
+import type { UserType } from '../types/user.types';
+import { getSession, setSession } from './auth.utils';
+import { loginByToken } from '../services/auth.service';
 
 type AuthStateType = {
-    user: UserType | null,
-    isInitialized: boolean
-}
+  user: UserType | null;
+  isInitialized: boolean;
+};
 
 type AuthContextType = AuthStateType & {
-    setUser: (user: UserType) => void
-    isAuthonticated: boolean
-}
+  setUser: (user: UserType) => void;
+  isAuthenticated: boolean;
+};
 
-export const AuthContext = createContext<AuthContextType | null>(null)
+export const AuthContext = createContext<AuthContextType | null>(null);
 
 type Props = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
 export const AuthProvider = ({ children }: Props) => {
-    const [authState, setAuthState] = useState<AuthStateType>({ user: null, isInitialized: false })
+  const [authState, setAuthState] = useState<AuthStateType>({
+    user: null,
+    isInitialized: false,
+  });
 
-    const setUser = useCallback((user: UserType) => {
-        setAuthState((prev) => ({ ...prev, user }))
-    }, [])
+  const setUser = (user: UserType) => {
+    setAuthState({ ...authState, user });
+  };
 
-    useEffect(() => {
-        const initialize = async () => {
-            const token = getSession()
-            try {
-                if (token) {
-                    const user = await loginByToken(token);
-                    setSession(token)
-                    setAuthState((prev) => ({ ...prev, isInitialized: true, user }))
-                } else {
-                    throw Error('Unauthorized')
-                }
-            } catch (error) {
-                setAuthState((prev) => ({ ...prev, isInitialized: true }))
-            }
+  useEffect(() => {
+    const initialize = async () => {
+      const token = getSession();
+      try {
+        if (token) {
+          const user = await loginByToken(token);
+          setSession(token);
+          setAuthState((prev) => ({ ...prev, user }));
         }
-        initialize()
-    }, [])
+      } catch (error) {
+        console.error('initialize auth failed', error);
+      } finally {
+        setAuthState((prev) => ({ ...prev, isInitialized: true }));
+      }
+    };
+    initialize();
+  }, []);
 
-    return <AuthContext.Provider value={{ ...authState, setUser, isAuthonticated: !!authState.user }}>
-        {children}
+  return (
+    <AuthContext.Provider
+      value={{ ...authState, setUser, isAuthenticated: !!authState.user }}
+    >
+      {children}
     </AuthContext.Provider>
-}
+  );
+};
